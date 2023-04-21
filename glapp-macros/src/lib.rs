7e098fa8294f855fdfd4e6674d7a2e6fba3e64f6
Mikael Lindqvist <li.mikael@gmail.com>
2023-04-22 00:31:01 +0800

    fixed macro stuff

--- glapp-macros/src/lib.rs
+++ glapp-macros/src/lib.rs
@@ -3,7 +3,18 @@ use quote::{quote, format_ident};
 use syn::{parse_macro_input, ItemFn};
 
 #[proc_macro_attribute]
-pub fn glapp_main(_attr: TokenStream, input: TokenStream) -> TokenStream {
+pub fn glapp_main(attr: TokenStream, input: TokenStream) -> TokenStream {
+	let mut parent=quote!();
+	for a in attr {
+		match a {
+			TokenTree::Ident(i)=>{
+				let i=proc_macro2::Ident::new(&*i.to_string(),proc_macro2::Span::call_site());
+				parent.extend(quote!(#i));
+			},
+			_=>{panic!("expected ident as parent")}
+		}
+	}
+
 	let mut ast=parse_macro_input!(input as ItemFn);
 	ast.sig.ident=format_ident!("_glapp_main_{}",ast.sig.ident.clone().to_string());
 	let name=ast.sig.ident.clone();
@@ -18,16 +29,16 @@ pub fn glapp_main(_attr: TokenStream, input: TokenStream) -> TokenStream {
 		out.extend(quote!{
 			#[cfg(not(target_os="android"))]
 			pub fn main() {
-				let b=::glapp::app_window_glutin::AppWindowBuilderGlutin::new();
-				#name(::glapp::App::new(b));
+				let b=#parent::glapp::app_window_glutin::AppWindowBuilderGlutin::new();
+				#name(#parent::glapp::App::new(b));
 			}
 
 			#[cfg(target_os="android")]
 			#[no_mangle]
 			pub fn android_main(android_app: ::glapp::app_window_glutin::AndroidApp) {
-				let b=::glapp::app_window_glutin::AppWindowBuilderGlutin::new();
+				let b=#parent::glapp::app_window_glutin::AppWindowBuilderGlutin::new();
 				b.with_android_app(android_app);
-				#name(::glapp::App::new(b));
+				#name(#parent::glapp::App::new(b));
 			}
 		});
 	}
@@ -36,16 +47,16 @@ pub fn glapp_main(_attr: TokenStream, input: TokenStream) -> TokenStream {
 		out.extend(quote!{
 			#[cfg(not(target_os="android"))]
 			pub fn main() {
-				let b=::glapp::app_window_sdl::AppWindowBuilderSdl::new();
-				#name(::glapp::App::new(b));
+				let b=#parent::glapp::app_window_sdl::AppWindowBuilderSdl::new();
+				#name(#parent::glapp::App::new(b));
 			}
 
 			#[cfg(target_os="android")]
 			#[no_mangle]
 			#[allow(non_snake_case)]
 			pub fn SDL_main() {
-				let b=::glapp::app_window_sdl::AppWindowBuilderSdl::new();
-				#name(::glapp::App::new(b));
+				let b=#parent::glapp::app_window_sdl::AppWindowBuilderSdl::new();
+				#name(#parent::glapp::App::new(b));
 			}
 		});
 	}
